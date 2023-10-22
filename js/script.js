17d367d04f183097bde1f7d747dad677cfb6dd8a
Juan Marcos Marascutti <marascuttijm@sersoc.org.ar>
2023-10-22 09:32:07 -0300

    Se finaliza compra de tickets

--- js/script.js
+++ js/script.js
@@ -1,58 +1,99 @@
 function limpiarCampos() {
-    document.getElementById('nombre').value = '';
-    document.getElementById('apellido').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('cantidad').value = '';
-    document.getElementById('categoria').value = 'Estudiante';
-    document.getElementById('importeTotal').innerHTML = 'Total a Pagar: $';
+  document.getElementById("nombre").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("cantidad").value = "";
+  document.getElementById("categoria").value = "Estudiante";
+  document.getElementById("importeTotal").innerHTML = "Total a Pagar: $";
 }
-
 
 const MENSAJES_VALIDACIONES = {
-    NOMBRE_VACIO: "Por favor, ingrese su nombre",
-    APELLIDO_VACIO: "Por favor, ingrese su apellido",
-    EMAIL_VACIO: "Por favor, ingrese su correo electrónico",
-    CANTIDAD_ENTRADAS_VACIO: "Por favor, ingrese la cantidad de entradas"
-}
-
+  NOMBRE_VACIO: "Por favor, ingrese su nombre",
+  APELLIDO_VACIO: "Por favor, ingrese su apellido",
+  EMAIL_VACIO: "Por favor, ingrese su correo electrónico",
+  CANTIDAD_ENTRADAS_VACIO: "Por favor, ingrese la cantidad de entradas",
+  CANTIDAD_ENTRADAS_NUMEROS:
+    "Por favor, debe ingresar una cantidad de entradas expresadas en números enteros mayor a 0",
+};
 
 function obtenerResumen() {
-    if (validarCampos()) {
-       alert("paso todo okey")
-    } 
+  if (validarCampos()) {
+    obtenerTotal();
+  }
 }
 
 function validarCampos() {
-    var ok = true; 
-    var campoVacio = null;
-    
-    var nombre = document.getElementById('nombre');
-    var apellido = document.getElementById('apellido');
-    var email = document.getElementById('email');
-    var cantidad = document.getElementById('cantidad');
+  var ok = true;
+  var campoVacio = null;
 
-    if (nombre.value == "") {
-        alert(MENSAJES_VALIDACIONES.NOMBRE_VACIO);
-        campoVacio = nombre;
-        ok = false; 
-    } 
-    else if (apellido.value == "") {
-        alert(MENSAJES_VALIDACIONES.APELLIDO_VACIO);
-        campoVacio = apellido;
-        ok = false;
-    } else if (email.value == "") {
-        alert(MENSAJES_VALIDACIONES.EMAIL_VACIO);
-        campoVacio = email;
-        ok = false;
-    } else if (cantidad.value == "") {
-        alert(MENSAJES_VALIDACIONES.CANTIDAD_ENTRADAS_VACIO);
-        campoVacio = cantidad;
-        ok = false;
-    }
+  var nombre = document.getElementById("nombre");
+  var apellido = document.getElementById("apellido");
+  var email = document.getElementById("email");
+  var cantidad = document.getElementById("cantidad");
 
-    if (campoVacio) {
-        campoVacio.focus(); 
+  if (nombre.value == "") {
+    alert(MENSAJES_VALIDACIONES.NOMBRE_VACIO);
+    campoVacio = nombre;
+    ok = false;
+  } else if (apellido.value == "") {
+    alert(MENSAJES_VALIDACIONES.APELLIDO_VACIO);
+    campoVacio = apellido;
+    ok = false;
+  } else if (email.value == "") {
+    alert(MENSAJES_VALIDACIONES.EMAIL_VACIO);
+    campoVacio = email;
+    ok = false;
+  } else if (cantidad.value == "") {
+    alert(MENSAJES_VALIDACIONES.CANTIDAD_ENTRADAS_VACIO);
+    campoVacio = cantidad;
+    ok = false;
+  }
+
+  if (campoVacio) {
+    campoVacio.focus();
+  }
+
+  return ok;
+}
+
+function obtenerTotal() {
+  var cantEntradas,
+    total,
+    categoria,
+    entradaEstudiante,
+    entradaTrainee,
+    entradaJr,
+    general;
+
+  cantEntradas = document.getElementById("cantidad").value;
+  categoria = document.getElementById("categoria").value;
+
+  entradaEstudiante = 200 - (200 * 80) / 100;
+  entradaTrainee = 200 - (200 * 50) / 100;
+  entradaJr = 200 - (200 * 15) / 100;
+  general = 200;
+
+  if (cantEntradas > 0 && !isNaN(cantEntradas) && cantEntradas % 1 == 0) {
+    if (categoria == "Estudiante") {
+      total = entradaEstudiante * cantEntradas;
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: $" + total;
+    } else if (categoria == "Trainee") {
+      total = entradaTrainee * cantEntradas;
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: $" + total;
+    } else if (categoria == "Junior") {
+      total = entradaJr * cantEntradas;
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: $" + total;
+    } else if (categoria == "General") {
+      total = general * cantEntradas;
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: $" + total;
     }
-    
-    return ok;
+  } else {
+    window.alert(MENSAJES_VALIDACIONES.CANTIDAD_ENTRADAS_NUMEROS);
+    document.getElementById("cantidad").focus();
+
+  }
 }
